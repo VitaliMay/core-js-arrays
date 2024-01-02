cd308c4428f975888214e2cf307d0e5ee41d88fe
VitaliMay <mvt30@mail.ru>
2024-01-02 22:07:16 +0300

    feat: start task 01-findElement

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -19,9 +19,12 @@
  *    ['Array', 'Number', 'string'], 'Date'    => -1
  *    [0, 1, 2, 3, 4, 5], 5    => 5
  */
-function findElement(/* arr, value */) {
-  throw new Error('Not implemented');
+function findElement(arr, value) {
+  return arr.indexOf(value);
 }
+// function findElement(/* arr, value */) {
+//   throw new Error('Not implemented');
+// }
 
 /**
  * Generates an array of odd numbers of the specified length
