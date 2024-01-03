fa0e1cd3ffe729e47d8fb5a0d0d80f5b09923f0c
VitaliMay <mvt30@mail.ru>
2024-01-03 19:38:04 +0300

    feat: tasks 10-insertItem 11-getHead

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -188,7 +188,6 @@ function isSameLength(arr) {
 function isValueEqualsIndex(arr) {
   return arr.some((item, index) => item === index);
 }
-// не коммитил
 
 /**
  * Inserts the item into specified array at specified index.
@@ -201,8 +200,8 @@ function isValueEqualsIndex(arr) {
  *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
  *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  return arr.splice(index, 0, item);
 }
 
 /**
@@ -216,8 +215,8 @@ function insertItem(/* arr, item, index */) {
  *    getHead([ 'a', 'b', 'c', 'd'], 3) => [ 'a', 'b', 'c' ]
  *    getHead([ 'a', 'b', 'c', 'd'], 0) => []
  */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  return arr.slice(0, n);
 }
 
 /**
